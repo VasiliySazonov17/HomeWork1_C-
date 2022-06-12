71452f223314406bc03f8ce58a2a926e445e5e6b
Vasiliy <vasiliyasazonov@gmail.com>
2022-06-13 02:24:05 +0300

    add program max_min

--- Program.cs
+++ Program.cs
@@ -3,3 +3,28 @@
 //second branch - max_numb
 //third branch - even_or_odd
 //fourth branch - row_even
+
+Console.Write("Input number first: ");
+int num_1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Input number second: ");
+int num_2 = Convert.ToInt32(Console.ReadLine());
+
+
+
+if (num_1 < num_2)
+{
+    Console.WriteLine("Max_num = " + num_2);
+    Console.WriteLine("Min_num = " + num_1);
+     }
+
+else if (num_2 < num_1)
+
+{
+    Console.WriteLine("Max_num = " + num_1);
+    Console.WriteLine("Min_num = " + num_2);
+    }
+else if (num_2 == num_1)
+{
+    Console.WriteLine("Input different numbers");
+    }
