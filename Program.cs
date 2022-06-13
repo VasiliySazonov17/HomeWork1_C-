02593a4ff8d38eeb9b67dc551c5dbc69c20105e7
Vasiliy <vasiliyasazonov@gmail.com>
2022-06-13 14:12:33 +0300

    add program even or odd

--- Program.cs
+++ Program.cs
@@ -3,3 +3,18 @@
 //second branch - max_numb
 //third branch - even_or_odd
 //fourth branch - row_even
+
+Console.Write("Input number: ");
+int num_1 = Convert.ToInt32(Console.ReadLine());
+
+if (num_1 % 2 == 0)
+{
+    Console.Write("Number " + "'"+num_1+"'" + " even");
+}
+else 
+{
+    Console.Write("Number " + "'"+num_1+"'" + " odd");
+}
+
+
+
