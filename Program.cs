52f8d9657afbda6c2d075a7c44484768c57990b8
Vasiliy <vasiliyasazonov@gmail.com>
2022-06-13 14:28:01 +0300

    add program row_even

--- Program.cs
+++ Program.cs
@@ -3,3 +3,22 @@
 //second branch - max_numb
 //third branch - even_or_odd
 //fourth branch - row_even
+
+Console.Write("Input number: ");
+int num_1 = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+while (count <= num_1)
+{
+    if(count % 2 == 0)
+    {
+        Console.Write(count);
+        
+        if (1 < num_1 - count)
+        {
+            Console.Write(", ");
+        }
+    }
+    count ++;
+}
+
